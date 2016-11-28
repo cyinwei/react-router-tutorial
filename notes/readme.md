109ba374dd0ac9711ad4b32579f4149669817a6b
Yinwei (Charlie) Zhang <yinwei.c.zhang@gmail.com>
2016-11-27 21:13:25 -0600

    Done with tutorial 3 on Links!

---
+++ notes/readme.md
@@ -0,0 +1,15 @@
+
+
+### Links
+Links are components that functions as `<a href='route'></a>` tags.  To use
+them, use `<Link to='/route'></Link>` in a component.  Remember to import
+`Link` from 'react-router'.
+
+#### Example
+```js
+// ...
+import { Link } from 'react-router'
+
+// .. in App
+<Link to='/route'>link_name</Link>
+```
--- walkthru/modules/App.js
+++ walkthru/modules/App.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+      <div>Hello, React Router!
+        <ul role="nav">
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/repos">Repos</Link></li>
+        </ul>
+      </div>
+    )
   }
 })
